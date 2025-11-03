5b8f62bc3ef143e8aa5a820040803c2824976ef7
aalhajmee <a.alhajmee@gmail.com>
2025-11-03 16:28:39 -0500

    Remove unused files: tabs (homepage, notes, explore), duplicate data files, ETL scripts, bash script

--- tabs/explore.R
+++
@@ -1,9 +0,0 @@
-library(leaflet)
-
-tabItem(tabName = "explore",
-        h2("Explore"),
-        p("Dive into the data and uncover insights."),
-        leaflet() %>% setView(lng = -97.476204, lat = 39.293357, zoom = 4) %>% 
-          addTiles()
-)
-
